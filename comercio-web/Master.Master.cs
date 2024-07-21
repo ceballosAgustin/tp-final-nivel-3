b92f4d9e16b4340abf760ee500017971efe3b7ec
Agustín Ceballos <aceballos1923@gmail.com>
2024-07-21 02:29:38 -0300

    feat: Diseños y Seguridad
    Se agregan iconos y cambios en el diseño. Se implementa una clase Seguridad para hacer chequeos de sesión y tipo de usuario.

--- comercio-web/Master.Master.cs
+++ comercio-web/Master.Master.cs
@@ -4,6 +4,8 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using dominio;
+using negocio;
 
 namespace comercio_web
 {
@@ -11,7 +13,32 @@ namespace comercio_web
     {
         protected void Page_Load(object sender, EventArgs e)
         {
+            imgAvatar.ImageUrl = "https://simg.nicepng.com/png/small/202-2022264_usuario-annimo-usuario-annimo-user-icon-png-transparent.png";
 
+            if (!(Page is Default || Page is Registro || Page is Error))
+            {
+                // Sin sesión activa, a loguearse o registrarse
+                if (!(Seguridad.sesionActiva(Session["user"])))
+                {
+                    Response.Redirect("Default.aspx", false);
+                }
+                else
+                {
+                    // Con sesión activa, la guardo
+                    User user = (User)Session["user"];
+
+                    if (!string.IsNullOrEmpty(user.UrlImagenPerfil))
+                    {
+                        imgAvatar.ImageUrl = "~/Images/" + user.UrlImagenPerfil;
+                    }
+                }
+            }
+        }
+
+        protected void btnSalir_Click(object sender, EventArgs e)
+        {
+            Session.Clear();
+            Response.Redirect("Default.aspx");
         }
     }
 }
---
+++ negocio/Seguridad.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using dominio;
+
+namespace negocio
+{
+    public static class Seguridad
+    {
+        public static bool sesionActiva(object usuario)
+        {
+            // Usuario distinto de null lo dejo como usuario y sino como nulo
+            User user = usuario != null ? (User)usuario : null;
+
+            // Distinto de null y si el ID no es 0 (ya existe)
+            if(user != null && user.Id != 0)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        public static bool esAdmin(object usuario)
+        {
+            User user = usuario != null ? (User)usuario : null;
+
+            // Si no es nulo, devuelvo un Admin y sino un Usuario normal
+            return user != null ? user.Admin : false;
+        }
+    }
+}
