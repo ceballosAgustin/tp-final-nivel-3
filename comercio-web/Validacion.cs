fb5e3236357def2623fbf3a24c2f8cdcdf3f25e4
Agustín Ceballos <aceballos1923@gmail.com>
2024-08-05 04:52:32 -0300

    feat: Se agregan mejoras en método de Validación
    Se cambia el método antigüo para validar un texto vacío a una versión más intuitiva

--- comercio-web/Validacion.cs
+++ comercio-web/Validacion.cs
@@ -8,22 +8,38 @@ namespace comercio_web
 {
     public static class Validacion
     {
-        public static bool validaTextoVacio(object control)
-        {
-            if (control is TextBox texto)
-            {
-                // Si el texto está nulo o vacío
-                if (string.IsNullOrEmpty(texto.Text))
-                {
-                    return true;
-                }
-                else
-                {
-                    return false;
-                }
-            }
+        //public static bool validaTextoVacio(object control)
+        //{
+        //    if (control is TextBox texto)
+        //    {
+        //        // Si el texto está nulo o vacío
+        //        if (string.IsNullOrEmpty(texto.Text))
+        //        {
+        //            return true;
+        //        }
+        //        else
+        //        {
+        //            return false;
+        //        }
+        //    }
 
-            return false;
+        //    return false;
+        //}
+
+        public static bool validaTextoVacio(TextBox textBox, Label errorLabel, string mensaje)
+        {
+            if(string.IsNullOrWhiteSpace(textBox.Text))
+            {
+                errorLabel.Text = mensaje;
+                errorLabel.Visible = true;
+                return true;
+            }
+            else
+            {
+                errorLabel.Visible = false;
+                return false;
+            }
         }
+
     }
 }
